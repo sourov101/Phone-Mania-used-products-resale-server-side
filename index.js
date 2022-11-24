59cf62db319b31a62b57b5fa1c1274c7f71c0f52
sourov <sourov.jsr98@gmail.com>
2022-11-24 23:50:25 +0600

    brand sent from server

--- index.js
+++ index.js
@@ -17,8 +17,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const brandCollection = client.db('phone').collection('brands');
 
+        app.get('/brands', async (req, res) => {
+            const query = {};
+            const brands = await brandCollection.find(query).toArray();
+            res.send(brands);
+        })
+    }
+    finally {
 
+    }
+}
+run().catch(console.log)
 
 app.get('/', async (req, res) => {
     res.send('phone mania server is running')
