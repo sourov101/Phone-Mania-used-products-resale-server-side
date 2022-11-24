96d32a82719375f562132057bf89180e1dedf372
sourov <sourov.jsr98@gmail.com>
2022-11-24 20:09:57 +0600

    mongodb connection added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.ehoamog.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 
 app.get('/', async (req, res) => {
